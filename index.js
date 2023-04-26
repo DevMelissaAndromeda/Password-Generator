7f03a33fb4ac9dafbc56a00b95fb3fce66b8da3f
ReimuHKJ <cr60hkj27@gmail.com>
2023-04-26 18:31:38 +0300

    feat[HTML, CSS, JS]
    Fixed spacing and styling issues.
    Password Fields work and look properly.
    Values in Input Fields will now clamp properly from Keyboard Input.

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const characters =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-const passwordCount = document.getElementById("password-count");
-const characterCount = document.getElementById("character-count");
+const passwordCountEl = document.getElementById("password-count");
+const characterCountEl = document.getElementById("character-count");
 
-const generatedPasswords = document.getElementById("generated-passwords");
+const generatedPasswordsEl = document.getElementById("generated-passwords");
 
+
+const clamp = (number, min, max) => Math.min( Math.max(number, min), max );
+
+function validateInputRange(inputEl, min, max)
+{
+    inputEl.value = clamp(inputEl.value, min, max);
+}
 
 function clearPreviousPasswords()
 {
-    generatedPasswords.innerHTML = "";
+    generatedPasswordsEl.innerHTML = "";
 }
 
 function addNewPassword(newPassword)
 {
-    let generatedPasswordField = document.createElement("generated-password-field").textContent = newPassword;
-    generatedPasswords.append(generatedPasswordField);
+    let newParagraphEl = document.createElement("p");
+    const textNodeEl = document.createTextNode(newPassword);
+
+    newParagraphEl.append(textNodeEl);
+
+    generatedPasswordsEl.append(newParagraphEl);
+
+    newParagraphEl.className += "generated-password-field";
 }
 
 function generatePasswords()
 {
     clearPreviousPasswords();
 
-    for (let i = 0; i < passwordCount.value; i++)
+    for (let i = 0; i < passwordCountEl.value; i++)
     {
         let newPassword = "";
         
-        for (let j = 0; j < characterCount.value; j++)
+        for (let j = 0; j < characterCountEl.value; j++)
         {
             let randomlyChosenCharacter = characters[ Math.floor( Math.random() * characters.length ) ];
             newPassword += randomlyChosenCharacter;
