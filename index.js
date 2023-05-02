f30bef8fa8bcb6de19e606d0a5448329a8b1ed36
ReimuHKJ <cr60hkj27@gmail.com>
2023-05-02 20:36:56 +0300

    fix[HTML, CSS, JS]: Fixes for previous breaking changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-const centeredBodyEl = document.getElementById('centered-body');
+const centeredBodyEl = document.getElementById('actual-body');
 const passwordCountEl = document.getElementById("password-count");
 const characterCountEl = document.getElementById("character-count");
 const generatedPasswordsEl = document.getElementById("generated-passwords");
@@ -12,13 +12,13 @@ const clamp = (number, min, max) => Math.min( Math.max(number, min), max );
 
 function popup(message)
 {
-    const old_PopupEl_Instance_To_Delete = document.getElementById("popup");
-    centeredBodyEl.removeChild(old_PopupEl_Instance_To_Delete);
+    const old_PopupEl_Instance_To_Delete    = document.getElementById("popup");
+    old_PopupEl_Instance_To_Delete          .remove();
 
-    const new_PopupEl_Instance  = document.createElement('h1');
-    new_PopupEl_Instance        .textContent = message;
-    new_PopupEl_Instance        .id = 'popup';
-    new_PopupEl_Instance        .style.animation = 'popup 3s forwards';
+    const   new_PopupEl_Instance    = document.createElement('h1');
+            new_PopupEl_Instance    .textContent = message;
+            new_PopupEl_Instance    .id = 'popup';
+            new_PopupEl_Instance    .style.animation = 'popup 3s forwards';
 
     centeredBodyEl.appendChild(new_PopupEl_Instance);
 }
